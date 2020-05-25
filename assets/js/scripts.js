e76ff03448e022074cfcc3cd776e4702153b8817
Clifford Fung <fung.clifford@gmail.com>
2020-05-24 18:57:03 -0700

    fixed styling and added about me content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 var topButton = document.getElementById("top-button");
 var navBar = document.getElementById("navbar-container");
-var navBarLi = document.getElementsByClassName("navbar-container-li");
+var navBarItems = document.getElementsByClassName("navbar-container-items");
 
 window.onscroll = function() {onScroll()};
 
@@ -22,19 +22,19 @@ function topOfPage() {
 // change navbar background and display button on scroll
 function onScroll() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      console.log(navBarLi);
+      console.log(navBarItems);
       topButton.style.display = "block";
       navBar.style.backgroundColor = "white";
-      navBar.style.borderBottom = "1px solid rgba(0,0,0,.2)";
-      for (let i=0; i < navBarLi.length; i++) {
-        navBarLi[i].style["color"] = "black";
+      navBar.style.boxShadow = "0 0 20px 0 #ccc";
+      for (let i=0; i < navBarItems.length; i++) {
+        navBarItems[i].style["color"] = "black";
       }
     } else {
       topButton.style.display = "none";
       navBar.style.backgroundColor = "transparent";
-      navBar.style.borderBottom = "none";
-      for (let i=0; i < navBarLi.length; i++) {
-        navBarLi[i].style["color"] = "white";
+      navBar.style.boxShadow = "none";
+      for (let i=0; i < navBarItems.length; i++) {
+        navBarItems[i].style["color"] = "white";
       }
     }
   }
