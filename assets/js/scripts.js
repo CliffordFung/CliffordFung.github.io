11c4fb1a05d36a12e36fda3f4f6619ff1c887428
Clifford Fung <fung.clifford@gmail.com>
2020-06-10 20:08:05 -0700

    refactor css and removed px

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,3 @@
-var topButton = document.getElementById("top-button");
-var navBar = document.getElementById("navbar-container");
-var navBarItems = document.getElementsByClassName("navbar-container-items");
-
-window.onscroll = function() {onScroll()};
 
 // water ripple effect
 // jQuery(document).ready(function(){
@@ -13,19 +8,25 @@ window.onscroll = function() {onScroll()};
 //   });
 // });
 
+// return to top button
+var topButton = document.getElementById("top-button");
+window.onscroll = function() {onScroll()};
 // return to top of the page when top-button is clicked
 function topOfPage() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+// navigation bar style on scroll
+var navBar = document.getElementById("navbar-container");
+var navBarItems = document.getElementsByClassName("navbar-container-items");
 // change navbar background and display button on scroll
 function onScroll() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       console.log(navBarItems);
       topButton.style.display = "block";
       navBar.style.backgroundColor = "white";
-      navBar.style.boxShadow = "0 0 20px 0 #ccc";
+      navBar.style.boxShadow = "0 0 1.5rem 0 #ccc";
       for (let i=0; i < navBarItems.length; i++) {
         navBarItems[i].style["color"] = "black";
       }
