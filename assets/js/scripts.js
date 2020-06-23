0ebdf4e57a9d62615567ff7f5b95a514c36fe6b9
Clifford Fung <fung.clifford@gmail.com>
2020-06-22 21:23:37 -0700

    refactored nav bar to be responsive for smaller screens

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,33 +9,55 @@
 
 /* return to top button */
 var topButton = document.getElementById("top-button");
-window.onscroll = function() {onScroll()};
+window.onscroll = function () {
+  onScroll();
+};
 // return to top of the page when top-button is clicked
 function topOfPage() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 /* navigation bar style on scroll */
 var navBar = document.getElementById("navbar-container");
 var navBarItems = document.getElementsByClassName("navbar-container-items");
+var navBarHamMenu = document.getElementsByClassName("hamburger-menu-bar");
+var toggleHamMenu = document.getElementsByClassName("hamburger-menu")[0];
+var displayNavBar = document.getElementsByClassName("toggle-navbar-display");
+
+// prevent click to go back to top of the page
+$(".hamburger-menu").click(function (e) {
+  e.preventDefault();
+});
+
+// toggle hamburger menu to become active
+toggleHamMenu.addEventListener("click", () => {
+  for (let i = 0; i < displayNavBar.length; i++) {
+    displayNavBar[i].classList.toggle("active");
+  }
+});
+
 // change navbar background and display button on scroll
 function onScroll() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      console.log(navBarItems);
-      topButton.style.display = "block";
-      navBar.style.backgroundColor = "white";
-      navBar.style.boxShadow = "0 0 1.5rem 0 #ccc";
-      for (let i=0; i < navBarItems.length; i++) {
-        navBarItems[i].style["color"] = "black";
-      }
-    } else {
-      topButton.style.display = "none";
-      navBar.style.backgroundColor = "transparent";
-      navBar.style.boxShadow = "none";
-      for (let i=0; i < navBarItems.length; i++) {
-        navBarItems[i].style["color"] = "white";
-      }
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    topButton.style.display = "block";
+    navBar.style.backgroundColor = "white";
+    navBar.style.boxShadow = "0 0 1.5rem 0 #ccc";
+    for (let i = 0; i < navBarItems.length; i++) {
+      navBarItems[i].style["color"] = "black";
+    }
+    for (let i = 0; i < navBarHamMenu.length; i++) {
+      navBarHamMenu[i].style["background-color"] = "black";
+    }
+  } else {
+    topButton.style.display = "none";
+    navBar.style.backgroundColor = "black";
+    navBar.style.boxShadow = "none";
+    for (let i = 0; i < navBarItems.length; i++) {
+      navBarItems[i].style["color"] = "white";
+    }
+    for (let i = 0; i < navBarHamMenu.length; i++) {
+      navBarHamMenu[i].style["background-color"] = "white";
     }
   }
-
+}
