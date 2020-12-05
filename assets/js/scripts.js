79b0108dee55c16a15975f5d2611b769e0c17a42
Clifford Fung <fung.clifford@gmail.com>
2020-12-05 14:38:10 -0800

    only to display ripple effect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 /* water ripple effect */
-// jQuery(document).ready(function(){
-//   $('.landing-page-container').ripples({
-//     dropRadius: 30,
-//     perturbance: 0.05,
-//     resolution: 720
-//   });
-// });
+jQuery(document).ready(function(){
+  $('.landing-page-container').ripples({
+    dropRadius: 30,
+    perturbance: 0.05,
+    resolution: 720
+  });
+});
 
 /* return to top button */
 var topButton = document.getElementById("top-button");
