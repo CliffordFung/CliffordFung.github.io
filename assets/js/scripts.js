d8e00b28a84a690c3aa5f284e13ca5c74b367fbf
Clifford Fung <cliffordfung@CLIR-00149.hitronhub.home>
2024-03-06 18:30:28 -0800

    updated formatter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,20 @@
 /* water ripple effect */
-jQuery(document).ready(function(){
-  $('.landing-page').ripples({
+jQuery(document).ready(function () {
+  $(".landing-page").ripples({
     dropRadius: 30,
     perturbance: 0.05,
-    resolution: 720
+    resolution: 720,
   });
 });
 
 /* skill bars */
-$(".bar").each(function(){
-  $(this).find(".bar-inner").animate({
-    width: $(this).attr("data-width")
-  },2000)
+$(".bar").each(function () {
+  $(this)
+    .find(".bar-inner")
+    .animate(
+      {
+        width: $(this).attr("data-width"),
+      },
+      2000
+    );
 });
