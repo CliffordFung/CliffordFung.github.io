1d532843ad9caef249590c3d39f37ed91d9ec89a
Clifford Fung <fung.clifford@gmail.com>
2024-03-07 19:39:19 -0800

    fix scroll behaviour

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,21 @@ jQuery(document).ready(function () {
     resolution: 720,
   });
 });
+
+const container = document.querySelector(".scroll-container");
+const items = document.querySelector(".slides");
+const child = document.querySelector(".right-column");
+
+container.addEventListener("wheel", (event) => {
+  event.preventDefault();
+  const delta = event.deltaY;
+
+  container.scrollBy({
+    top: delta,
+    behavior: "smooth",
+  });
+});
+
+child.addEventListener("wheel", (event) => {
+  event.stopPropagation();
+});
